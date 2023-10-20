6a8855a114d67cd848463aa482e27596992936b3
Sceler16 <147984391+Sceler16@users.noreply.github.com>
2023-10-20 13:37:47 +0200

    Update auxiliaryAPI.js

--- public/js/auxiliaryAPI.js
+++ public/js/auxiliaryAPI.js
@@ -1,19 +1,21 @@
-exports.celsiusToFahrenheit = function (celsius) {
-    return celsius * (9/5) + 32;
-}
-
-exports.fahrenheitToCelsius = function (fahrenheit) {
-    return (fahrenheit - 32) * (5/9);
-}
-
-exports.getGreetingDependOnTime = function (myDate) {
-    const timeBegin = 6;
-    const timeEnd = 22;
-    const currentHour = myDate.getHours();
-
-    if (currentHour >= timeBegin && currentHour < timeEnd) {
-        return "Guten Morgen";
-    } else {
-        return "Gute Nacht";
-    }
-}
+/* conversion from celsius to fahrenheit scale */
+exports.celsiusToFahrenheit = function (x){
+    return x*(9/5) + 32
+   }
+   /* conversion from fahrenheit to celsius scale.  Removed unnecessary condition */
+   exports.fahrenheitToCelcius = function (fahrenheit){
+       return (fahrenheit - 32) * (5 / 9);
+    }   
+   
+   /* getting the current date and time, returning the appropriate message */ 
+   exports.getGreetingDependOnTime =  function (myDate) {
+       const timeBegin = 6;
+       const timeEnd = 22;
+       const currentHour = myDate.getHours();
+     
+       if (currentHour >= timeBegin && currentHour < timeEnd) {
+         return "Guten Morgen";
+       } else {
+         return "Gute Nacht";
+       }
+     }
